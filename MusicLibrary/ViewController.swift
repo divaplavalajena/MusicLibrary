ae3aa9e2a1ecc9f742227b106a4ad0b5bb9b778b
Jena Grafton <JenaLGrafton@gmail.com>
2016-09-28 16:18:20 -0500

    Initial Commit

---
+++ MusicLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicLibrary
+//
+//  Created by Jena Grafton on 9/28/16.
+//  Copyright © 2016 Bella Voce Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
